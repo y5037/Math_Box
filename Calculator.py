1e19d6a73470bbc7c813824f826d8c0c072f2f23
y5037 <71241364+y5037@users.noreply.github.com>
2024-10-29 12:10:39 +0900

    Add say_hello function

--- Calculator.py
+++ Calculator.py
@@ -8,9 +8,8 @@ def subtract(a, b):
 def multiply(a, b):
 	return a*b
 
-<<<<<<< HEAD
 def divide_premium(a, b):
-=======
-def divide_free(a, b):
->>>>>>> main
 	return a/b
+
+def say_hello():
+	print("hello")
